4451043452bba9e93ba50a1fd200872c70e00194
Alexander Yakovlev <33040934+AlexJameson@users.noreply.github.com>
2024-11-23 02:10:00 +0300

    upd regexps

--- antispam-mini.py
+++ antispam-mini.py
@@ -434,7 +434,6 @@ async def check_automatically(update: Update, context: CallbackContext):
         emoji_critical_num = False
 
     is_reply = message.reply_to_message is not None
-    is_premium = from_user.is_premium is not None
 	 
     for user in db_main.all():
         if chat_id in user['chats']:
@@ -445,7 +444,7 @@ async def check_automatically(update: Update, context: CallbackContext):
                 # Ban automatically
                 # todo: add repeated emojis check
 
-                if (len(words) < 500 and is_premium is True and is_reply is False) and (("✅✅✅✅" in words or "✅✅✅✅" in words.replace('\U0001F537', '✅') or (crit_tokens_bool is True ) or num_mixed > 1 or spam_tokens is not None or emoji_critical_num is True)):
+                if (len(words) < 500 and is_reply is False) and (("✅✅✅✅" in words or "✅✅✅✅" in words.replace('\U0001F537', '✅') or (crit_tokens_bool is True ) or num_mixed > 1 or spam_tokens is not None or emoji_critical_num is True)):
                     verdict = f"""
 <b>Основное регулярное выражению:</b> {spam_tokens is not None}
 <b>Критические токены:</b> {crit_tokens_bool} | {crit_tokens_string}
